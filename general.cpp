33001b1f183e076ca4d2a7b0935d4cde76b95e0b
leandropucrs <leandro-ie@hotmail.com>
2018-11-09 20:55:06 -0200

    9-11-18

---
+++ general.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int main(){
+
+int jogadas, jogadas1, jogadas2, jogadas3, jogadas4;
+int dado1, dado2, dado3, dado4, dado5;
+int jogador1, jogador2, jogador3, jogador4;
+int pontos;
+
+
+
+srand ( time(NULL) );
+dado1 = rand() % 6;
+dado2 = rand() % 6;
+dado3 = rand() % 6;
+dado4 = rand() % 6;
+dado5 = rand() % 6;
+
+
+cout<< "  "<< dado1<<endl;
+cout<< "  "<< dado2<<endl;
+cout<< "  "<< dado3<<endl;
+cout<< "  "<< dado4<<endl;
+cout<< "  "<< dado5<<endl;
+
+
+
+
+};
